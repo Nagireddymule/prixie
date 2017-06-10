8e9d5da39996a3b3eee5c0f48533631ec2e9ed48
santhosh <santhu4399@gmail.com>
2017-06-10 16:05:36 +0530

    prixie

---
+++ prixieapi.js
@@ -0,0 +1,12 @@
+var express =require("express");
+var app = express();
+
+app.set('port',process.env.PORT||3000)
+
+app.get('/',function(req,res){
+  res.send('working');
+})
+
+app.listen(app.get('port'),function(){
+  console.log("prixie is running on port"+app.get('port'));
+});
