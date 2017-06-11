f8a6f243ea127e7dd597201497af6cb421f4f5b6
santhosh <santhu4399@gmail.com>
2017-06-11 15:18:35 +0530

    prixie

--- prixieapi.js
+++ prixieapi.js
@@ -1,4 +1,5 @@
 var express =require("express");
+var mysql=require("mysql");
 var MongoClient = require("mongodb").MongoClient;
 var app = express();
 var mongourl = "mongodb://localhost:27017/walkins";
@@ -20,16 +21,181 @@ app.get('/interview_schedules',function(req, res){
           });
     });
 });
-
 app.get('/consultancy',function(req, res){
-  res.send("consultancies List");
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+connection.connect();
+connection.query('select * from consultancy', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("consultancies List"+result);
 });
-app.get('/eligibility',function(req, res){
-  res.send("Eligibility");
 });
-app.get('/required_documents',function(req, res){
-  res.send("Documents required for interview");
+
+app.get('/written_test',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='WRITTEN TEST'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
 });
+});
+
+app.get('/technical_test',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='TECHNICAL'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+  });
+});
+
+app.get('/group_discussion',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='GD'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+app.get('/vercent_round',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='VERCENT ROUND'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+
+
+app.get('/jam',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='JAM'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+
+app.get('/system_programming',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='SYSTEM PROGRAMMING'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+
+app.get('/typing_test',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='TYPING TEST'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+
+app.get('/hr',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='HR(OPERATION)'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+
+app.get('/telephonic',function(req, res){
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'prixie'
+  });
+  connection.connect();
+  connection.query("select round_discription from rounds where round_name='TELEPHONIC ROUND'", function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+  var result = JSON.stringify(results);
+  connection.end();
+    res.send("rounds List"+result);
+});
+});
+
+
+
+
+
+
 
 app.get('/it_selection_process',function(req, res){
   res.send("selection procedure for IT recruitment");
@@ -38,21 +204,12 @@ app.get('/it_selection_process',function(req, res){
 app.get('/non_it_selection_process',function(req, res){
   res.send("selection procedure for Non-IT recruitment");
 });
-
-app.get('/written_test',function(req, res){
-  res.send("About written test");
+app.get('/eligibility',function(req, res){
+  res.send("Eligibility");
 });
+app.get('/required_documents',function(req, res){
+  res.send("Documents required for interview");
 
-app.get('/technical_test',function(req, res){
-  res.send("About technical test");
-});
-
-app.get('/group_discussion_and_jam',function(req, res){
-  res.send("About Group Discussion and JAM Rounds");
-});
-
-app.get('/varcent_round',function(req, res){
-  res.send("About vercent round");
 });
 
 app.get('/salary',function(req, res){
