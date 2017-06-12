7c6237f25664dd655b64d8c7bd3da1d0e8b08ca9
santhosh <santhu4399@gmail.com>
2017-06-12 14:42:10 +0530

    prixie

--- webhookapi.js
+++ webhookapi.js
@@ -33,12 +33,12 @@ app.get('/webhook/', function (req, res) {
   });
 
 app.post('/webhook', function (req, res) {
-  var data = req.body;
-  console.log("req body"+JSON.stringify(data));
+  //var data = req.body;
+//  console.log("req body"+JSON.stringify(data));
   if (data.object === 'page') {
     data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
+      //var pageID = entry.id;
+      //var timeOfEvent = entry.time;
       entry.messaging.forEach(function(event) {
         if (event.message) {
           receivedMessage(event);
@@ -55,12 +55,9 @@ app.post('/webhook', function (req, res) {
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
   var message = event.message;
-  var messageId = message.mid;
   var messageText = message.text;
   var messageAttachments = message.attachments;
-
   if (messageText) {
     switch (messageText) {
       case 'generic':
@@ -97,7 +94,6 @@ function callSendAPI(messageData) {
     if (!error && response.statusCode == 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
-
       console.log("Successfully sent generic message with id %s to recipient %s",
         messageId, recipientId);
     } else {
