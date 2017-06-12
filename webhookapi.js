177c44d85f7bc3850ca840efdad349368dc5809c
santhosh <santhu4399@gmail.com>
2017-06-12 12:38:47 +0530

    prixie

--- webhookapi.js
+++ webhookapi.js
@@ -22,18 +22,15 @@ app.get('/webhook/', function(req, res) {
     res.sendStatus(403);
   }
 });
+
+
+
 app.post('/webhook', function (req, res) {
   var data = req.body;
-
-  // Make sure this is a page subscription
   if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
     data.entry.forEach(function(entry) {
       var pageID = entry.id;
       var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
         if (event.message) {
           receivedMessage(event);
@@ -42,21 +39,9 @@ app.post('/webhook', function (req, res) {
         }
       });
     });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
     res.sendStatus(200);
   }
 });
-
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}
-
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -88,7 +73,6 @@ function receivedMessage(event) {
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
-
 
 function sendTextMessage(recipientId, messageText) {
   var messageData = {
