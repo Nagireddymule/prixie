1690420049ba6cf3f37ad8191da9111a912b78da
santhosh <santhu4399@gmail.com>
2017-06-14 18:47:45 +0530

    prixie

--- webhookapi.js
+++ webhookapi.js
@@ -105,7 +105,52 @@ function sendTextMessage(recipientId, messageText) {
                 "payload":"DEVELOPER_DEFINED_PAYLOAD"
               }
             ]
-          }
+          },
+          {
+           "title":"Welcome to Peter\'s Hats",
+           "image_url":"https://petersfancybrownhats.com/company_image.png",
+           "subtitle":"We\'ve got the right hat for everyone.",
+           "default_action": {
+             "type": "web_url",
+             "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+             "messenger_extensions": "true",
+             "webview_height_ratio": "tall",
+             "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+           },
+           "buttons":[
+               {
+               "type":"postback",
+               "title":"Start Chatting",
+               "payload":"DEVELOPER_DEFINED_PAYLOAD"
+               },{
+               "type":"postback",
+               "title":"Start Chatting",
+               "payload":"DEVELOPER_DEFINED_PAYLOAD"
+             }
+           ]
+         },{
+          "title":"Welcome to Peter\'s Hats",
+          "image_url":"https://petersfancybrownhats.com/company_image.png",
+          "subtitle":"We\'ve got the right hat for everyone.",
+          "default_action": {
+            "type": "web_url",
+            "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+            "messenger_extensions": "true",
+            "webview_height_ratio": "tall",
+            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+          },
+          "buttons":[
+              {
+              "type":"postback",
+              "title":"Start Chatting",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              },{
+              "type":"postback",
+              "title":"Start Chatting",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+            }
+          ]
+        }
         ]
       }
     }
