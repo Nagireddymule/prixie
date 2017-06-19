501e29e9b22196df97daa0f3f73a42a0f77ee46f
santhosh <santhu4399@gmail.com>
2017-06-19 11:18:29 +0530

    prixie

--- webhookapi.js
+++ webhookapi.js
@@ -222,25 +222,7 @@ function sendAttachmentMessage(recipientId, messageText) {
     callSendAPI(messageData);
 }
 
-/*function callSendAPI(messageData) {
-  console.log(messageData);
-  request({
-    uri: 'https://graph.facebook.com/v2.8/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: messageData
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.log(body);
-    }
-  });
-}*/
+
 app.listen(app.get('port'),function(){
 console.log("webhook is running on port "+app.get('port'));
 })
