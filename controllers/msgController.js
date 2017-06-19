f51ddc02e7c87300a5c90923555e2cf301322848
santhosh <santhu4399@gmail.com>
2017-06-19 16:22:16 +0530

    prixie

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -5,47 +5,53 @@ module.exports = function(event){
   if (event.postback) {
     console.log(event.postback);
     if (event.postback.payload == "GET_STARTED_PAYLOAD") {
-      var messageData ={
-  "recipient":{
-    "id":event.sender.id
-  },
-  "message":{
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Welcome to Interview Schedules",
-            "image_url":"https://preview.ibb.co/mSwUOQ/interview.png",
-            "subtitle":"@Interviews",
-            "buttons":[
-              {
-                "type":"postback",
-                "title":"Interviews",
-                "payload":"interview_schedules"
-              },
-            ]
-          },
-          {
-           "title":"Welcome to Tutorials",
-           "image_url":"https://preview.ibb.co/doe5xk/tutorials.jpg",
-           "subtitle":"@Tutorials",
-           "buttons":[
+      function getstart(){
+        var messageData ={
+    "recipient":{
+      "id":event.sender.id
+    },
+    "message":{
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
              {
-               "type":"postback",
-               "title":"Tutorials",
-               "payload":"Tutorials"
-             },
-           ]
-         }
-        ]
+              "title":"Welcome to Interview Schedules",
+              "image_url":"https://preview.ibb.co/mSwUOQ/interview.png",
+              "subtitle":"@Interviews",
+              "buttons":[
+                {
+                  "type":"postback",
+                  "title":"Interviews",
+                  "payload":"interview_schedules"
+                },
+              ]
+            },
+            {
+             "title":"Welcome to Tutorials",
+             "image_url":"https://preview.ibb.co/doe5xk/tutorials.jpg",
+             "subtitle":"@Tutorials",
+             "buttons":[
+               {
+                 "type":"postback",
+                 "title":"Tutorials",
+                 "payload":"Tutorials"
+               },
+             ]
+           }
+          ]
+        }
       }
     }
   }
-}
+  console.log("from getstart function");
+  callSendAPI(messageData);
+      }
+getstart();
 
-callSendAPI(messageData);
+
+
 
     }
 
