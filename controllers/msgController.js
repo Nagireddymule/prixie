9b91d358334efa7cc17c3c90dbbbfbc2de0f0cdd
santhosh <santhu4399@gmail.com>
2017-06-19 16:28:13 +0530

    prixie

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -5,57 +5,8 @@ module.exports = function(event){
   if (event.postback) {
     console.log(event.postback);
     if (event.postback.payload == "GET_STARTED_PAYLOAD") {
-      function getstart(){
-        var messageData ={
-    "recipient":{
-      "id":event.sender.id
-    },
-    "message":{
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type":"generic",
-          "elements":[
-             {
-              "title":"Welcome to Interview Schedules",
-              "image_url":"https://preview.ibb.co/mSwUOQ/interview.png",
-              "subtitle":"@Interviews",
-              "buttons":[
-                {
-                  "type":"postback",
-                  "title":"Interviews",
-                  "payload":"interview_schedules"
-                },
-              ]
-            },
-            {
-             "title":"Welcome to Tutorials",
-             "image_url":"https://preview.ibb.co/doe5xk/tutorials.jpg",
-             "subtitle":"@Tutorials",
-             "buttons":[
-               {
-                 "type":"postback",
-                 "title":"Tutorials",
-                 "payload":"Tutorials"
-               },
-             ]
-           }
-          ]
-        }
-      }
-    }
-  }
-  console.log("from getstart function");
-  callSendAPI(messageData);
-      }
 getstart();
-
-
-
-
     }
-
-
   }
   else if (event.message) {
     receivedMessage(event);
@@ -63,3 +14,48 @@ getstart();
     console.log(event);
   }
 }
+
+
+function getstart(){
+    var messageData ={
+      "recipient":{
+        "id":event.sender.id
+      },
+      "message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"generic",
+            "elements":[
+              {
+                "title":"Welcome to Interview Schedules",
+                "image_url":"https://preview.ibb.co/mSwUOQ/interview.png",
+                "subtitle":"@Interviews",
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title":"Interviews",
+                    "payload":"interview_schedules"
+                  },
+                ]
+              },
+              {
+                "title":"Welcome to Tutorials",
+                "image_url":"https://preview.ibb.co/doe5xk/tutorials.jpg",
+                "subtitle":"@Tutorials",
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title":"Tutorials",
+                    "payload":"Tutorials"
+                  },
+                ]
+              }
+            ]
+          }
+        }
+      }
+    }
+    console.log("from getstart function");
+    callSendAPI(messageData);
+  }
