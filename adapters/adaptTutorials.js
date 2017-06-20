fa29dcea992282b103cff191b3f2b795ab94d969
santhosh <santhu4399@gmail.com>
2017-06-20 17:43:49 +0530

    prixie

--- adapters/adaptTutorials.js
+++ adapters/adaptTutorials.js
@@ -5,7 +5,7 @@ request({
   method:"Get"
 },function(error,response){
 if(error) throw error;
-var tutList = JSON.parse(res.body);
+var tutList = JSON.parse(response.body);
 var tutListArray = [];
 for (var i = 0; i < 10; i++) {
   listarr.push({"content_type":"text","title":tutListArray[i].title,"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"})
