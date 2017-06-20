2f092e15fced00236e4d3bff02dfae79d55498b8
santhosh <santhu4399@gmail.com>
2017-06-20 17:45:32 +0530

    prixie

--- adapters/adaptTutorials.js
+++ adapters/adaptTutorials.js
@@ -8,7 +8,7 @@ if(error) throw error;
 var tutList = JSON.parse(response.body);
 var tutListArray = [];
 for (var i = 0; i < 10; i++) {
-  listarr.push({"content_type":"text","title":tutListArray[i].title,"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"})
+  tutListArray.push({"content_type":"text","title":tutList[i].title,"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"})
 }
 var tutListArrayData = JSON.stringify(tutListArray);
 return callback(tutListArrayData);
