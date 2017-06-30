28a3c344ab9e1818b0dc217ffc3e5065900cd549
navitha <navithareddy.nr@gmail.com>
2017-06-30 21:53:38 +0530

    prixie

--- controllers/receivedMessage.js
+++ controllers/receivedMessage.js
@@ -8,19 +8,24 @@ module.exports.receivedMessage = function(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var message = event.message;
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+
   if (event.postback){
     if (event.postback.payload == "GET_STARTED_PAYLOAD") {
       sendTextMessage(senderID, "hi");
     }
 
   }
-  else if (messageText) {
-        sendTextMessage(senderID, messageText);
+  if (event.message) {
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+    else if (messageText) {
+          sendTextMessage(senderID, messageText);
+      }
+    else if (messageAttachments) {
+        console.log("attachment came");
+        sendAttachmentMessage(senderID, messageText);
+      }
     }
-  else if (messageAttachments) {
-      console.log("attachment came");
-      sendAttachmentMessage(senderID, messageText);
-    }
+  }
+
 }
