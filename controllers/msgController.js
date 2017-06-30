cc3f57b32adfef34a394f8bc4f3360c7dc45eda8
navitha <navithareddy.nr@gmail.com>
2017-06-30 21:36:53 +0530

    prixie

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -15,7 +15,7 @@ module.exports.msgController= function(event){
     console.log(event.postback);
     if (event.postback.payload == "GET_STARTED_PAYLOAD") {
       var messageText = "hi";
-        this.getStartMenu(senderid);
+        //this.getStartMenu(senderid);
         sendTextMessage(senderid, messageText);
     }
     if (event.postback.payload == "interview_schedules") {
