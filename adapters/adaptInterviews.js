e77ede663c0399a4662cc0a2e81d78c8030f5687
navitha <navithareddy.nr@gmail.com>
2017-07-01 22:54:19 +0530

    prixie

--- adapters/adaptInterviews.js
+++ adapters/adaptInterviews.js
@@ -20,6 +20,7 @@ module.exports.adaptFilterSchedule = function(suburl,callback){
   },function(error,res){
     if(error) throw err;
     var responseData = JSON.parse(res.body);
+    console.log(responseData);
     var dataFormat = "";
     for (var i = 0; i < responseData.length; i++) {
       console.log(responseData[i]);
