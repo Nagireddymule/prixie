7ae651e36ad1921725dfdaeaecaf22a36f4966b8
santhosh <santhu4399@gmail.com>
2017-07-01 12:41:41 +0530

    prixie

--- adapters/adaptInterviews.js
+++ adapters/adaptInterviews.js
@@ -22,7 +22,7 @@ module.exports.adaptFilterSchedule = function(suburl,callback){
     var responseData = JSON.parse(res.body);
     var dataFormat = "";
     for (var i = 0; i < responseData.length; i++) {
-      dataFormat = ("Company: "+responseData[i].company+".\nWebsite: "+responseData[i].Website+"\nJob Role: "+responseData[i].Job_Role+"\nEligibility: "+responseData[i].Eligibility+"\nExperience: "+responseData[i].Experience+"\nJob Location: "+responseData[i].Job_location+"\nData: "+responseData[i].Walk_In_date+"\nTime: "+responseData[i].Walk_In_Time);
+      dataFormat = ("Company: "+responseData[i].company+".\nWebsite: "+responseData[i].Website+"\nJob Role: "+responseData[i].Job_Role+"\nEligibility: "+responseData[i].Eligibility);
     }
     return callback(dataFormat);
     //console.log(dataFormat);
