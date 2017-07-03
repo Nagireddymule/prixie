974c4d3e655ebc8d3ae4f4f6179f615927c763fc
santhosh <santhu4399@gmail.com>
2017-07-03 12:09:04 +0530

    prixie

--- adapters/adaptInterviews.js
+++ adapters/adaptInterviews.js
@@ -67,7 +67,9 @@ module.exports.adaptFilterSchedules = function(suburl,callback){
     method:"Get"
   },function(error,res){
     if(error) throw err;
-    console.log("response body ="+res.body);
+    if (res.body == "") {
+      console.log("NO more records found");
+    }
     var responseData = JSON.parse(res.body);
     //console.log("response data = "+responseData);
     var dataFormat = "";
