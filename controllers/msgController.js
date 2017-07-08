98e1ebbc79fd293ad2abc07f4801fd83c17d3074
santhosh <santhu4399@gmail.com>
2017-07-08 16:33:31 +0530

    prixie

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -522,7 +522,7 @@ module.exports.getFilterInterviewSchedulesByExpMinMax = function(suburl,expmin,e
                               {
                                 "type":"postback",
                                 "title":"Click here for more",
-                                "payload":"Expm-"+expmin+"-"+expmax+"-0"
+                                "payload":"Expmm-"+expmin+"-"+expmax+"-0"
                               },
                               {
                                 "type":"web_url",
