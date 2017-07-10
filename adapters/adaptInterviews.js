5cb9411469cf5ca3a9876114a05ec52c7172a8ef
santhosh <santhu4399@gmail.com>
2017-07-10 15:48:02 +0530

    prixie

--- adapters/adaptInterviews.js
+++ adapters/adaptInterviews.js
@@ -74,7 +74,7 @@ module.exports.adaptCompanyInfo = function(suburl,callback){
     }
     var responseData = JSON.parse(res.body);
     console.log(responseData);
-    var dataFormat = "Company Name :  "+responseData.company_name+"\n\nAddress :  "+responseData.address+"\n\nContact NO :  "+responseData.contact_number+"\nWebsite :  "+responseData.website;
+    var dataFormat = "Company Name :  "+responseData.company_name+"\n\nAddress :  "+responseData.address+"\n\nContact NO :  "+responseData.contact_number+"\n\nWebsite :  "+responseData.website;
     return callback(dataFormat);
   });
 }
