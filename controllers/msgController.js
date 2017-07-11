d204104ead947b746942f17edbc67018150d6b56
santhosh <santhu4399@gmail.com>
2017-07-11 12:00:21 +0530

    prixie

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -167,6 +167,7 @@ module.exports.getTutorialList = function(senderid){
   }
 module.exports.getCompanyInfo = function(senderid){
             adaptInterviews.adaptCompanyInfo("0",function(callback){
+              console.log("domain code = "+callback.domain_code);
               var messageData = {
                 "recipient":{
                   "id":senderid
